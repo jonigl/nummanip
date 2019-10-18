7c0bf24e33a00f55fadd7de0e7b07e1b5b3acb4e
Jonathan <jonigl@gmail.com>
2019-10-18 16:41:31 -0300

    v2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,10 +1,18 @@
 package calc
 
+import "errors"
+
 // Add() returns sum of integers
-func Add(numbers ...int) int {
+func Add(numbers ...int) (error, int) {
 	sum := 0
+
+	if len(numbers) < 2 {
+		return errors.New("provide more than 2 numbers"), sum
+	}
+
 	for _, num := range numbers {
 		sum += num
 	}
-	return sum
+
+	return nil, sum
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jonigl/nummanip
+module github.com/jonigl/nummanip/v2
 
 go 1.13
